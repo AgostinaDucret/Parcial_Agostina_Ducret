7cc8931ce2d824cb2594e3aaa8391da878e886f9
agosducret18@gmail.com <agosducret18@gmail.com>
2024-02-26 22:42:46 -0300

    Estructura Parcial

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+
+const Card = () => {
+  return (
+    <div>
+      <Form />
+      Hola, soy una tarjeta
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form>soy un form</form>
+    </div>
+  );
+};
+
+export default Form;
